dc21bb1eb3832c8f5d9fb8af3eafc364ff22bc95
Daniel <136662533+danielshanesilvestre@users.noreply.github.com>
2024-07-26 11:59:24 -0500

    Completed lab

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  function handleChange(event) {
+    console.log("Test");
+    setSelectedCategory(event.target.value);
+
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,9 +21,11 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {items.map((item) => {
+          return (selectedCategory == "All" || selectedCategory == item.category) ? (
+            <Item key={item.id} name={item.name} category={item.category} />
+          ) : null;
+        })}
       </ul>
     </div>
   );
